f2220150a2b71663eebf941ebfaeaf52947138c4
Habib oluwa <oluwahabib27@gmail.com>
2026-08-26 23:18:32 +0100

    updated

--- test_zodiac.py
+++ test_zodiac.py
@@ -61,7 +61,7 @@ def get_zodiac(month, day):
 
 
 # get all detail about the zodiac of aug 15 which is Leo
-# zodiac = get_zodiac(8, 15)
-# # print zodic with details
-# print(zodiac)
-# print(zodiac_data[zodiac])
+zodiac = get_zodiac(8, 15)
+# print zodic with details
+print(zodiac)
+print(zodiac_data[zodiac])
